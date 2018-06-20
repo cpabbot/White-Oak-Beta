f5e12ca8c5f6d4fd7f5a1a6020d0c0b2eb09c6e0
Cameron Abbot <cpabbot1@gmail.com>
2018-06-20 18:12:45 -0400

    fixing commit issues

--- js/scroll.js
+++ js/scroll.js
@@ -4,11 +4,13 @@ var isScrolling = false;
 var scrollY = 0;
 var lastScrollY = 0;
 var page = 0;
+var sunY = $(".sun").offset().top;
+var video = document.getElementById('brewerton-mp4');
 
 $(document).ready(function() {
     
-    var sunY = $(".sun").offset().top;
     vHeight = $(".second").offset().top;
+    video.load();
     
     var scrollIntervalID = setInterval(animate, 10);
     
@@ -21,12 +23,14 @@ $(document).ready(function() {
 });
 
 function animate() {
-    window.requestAnimationFrame(parallaxFixed($(".tree-background"), 0.3));
+//    window.requestAnimationFrame(parallaxFixed($(".tree-background"), 0.3));
+    parallaxFixed($(".tree-background"), 0.3, 0);
+    parallaxFixed($(".sun"), 0.05, sunY);
 }
 
-function parallaxFixed($el, amount) {
+function parallaxFixed($el, amount, offset) {
     var yPos = $(".main").scrollTop();
-    var newPos = -yPos*amount;
+    var newPos = -yPos*amount + offset;
     $el.css('top', newPos);
 }
 
@@ -36,53 +40,73 @@ function parallaxAbsolute() {
 //    $el.css('top', newPos);
 }
 
-function xxxparallax($el, amount, initialOffset) {
-    var yPos = $el.offset().top;
-//    var newPos = yPos*amount + initialOffset;
-    var newPos = -1*yPos + initialOffset;
-
-    $el.css('top', newPos);
-}
-
 function checkScroll() {
     if(!isScrolling) {
             scrollY = main.scrollTop();
             //alert(scrollY + " " + lastScrollY);
             if(scrollY > lastScrollY) { // scroll down
-                //alert("down");
                 scrollDown();
-                isScrolling = true;
             }
             else if(scrollY < lastScrollY) { // scroll up
-                //alert("up");
                 scrollUp();
-                isScrolling = true;
             }
         }
 }
 
 function scrollDown() {
-    page++;
-    scrollToPos(vHeight * page);
+    if(page == 3) { page += 6; }
+    else { page++; }
+    isScrolling = true;
+    scrollToPage(page);
 }
 
 function scrollUp() {
-    page--;
-    scrollToPos(vHeight * page);
+    if(page == 9) { page -= 6; }
+    else { page--; }
+    isScrolling = true;
+    scrollToPage(page);
 }
 
-function scrollToPos(newYPos) {
-    //alert(main.scrollTop() + "  to   " + newYPos + " (" + page + ")");
+function scrollToPage(page) {
+    var newYPos = Math.round(vHeight * page);
+//    alert(main.scrollTop() + "  to   " + newYPos + " (" + page + ")");
+    prepareBrewertonAnimation();
+    
     main.animate({
         scrollTop: newYPos
     }, 1000, function() {
+        pageLanded(page);
         setTimeout(function() {
             isScrolling = false
         }, 50);
-    })
+    });
     lastScrollY = newYPos;
 }
 
 function getDist() {
     return Math.abs($(".main").scrollTop());
+}
+
+function pageLanded(page) {
+    if(page == 0) {
+        video.pause();
+        video.currentTime = 0;
+        video.load();
+    }
+//    if(page == 1) {
+//        video.play();
+//    }
+    if(page == 2) {
+        video.pause();
+        video.currentTime = 0;
+        video.load();
+    }
+}
+
+function prepareBrewertonAnimation() {
+    if(page == 1) {
+        setTimeout(function() {
+            video.play();
+        }, 200);
+    }
 }
