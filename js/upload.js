663992578544f9b67715f440336e062d21022a72
Cameron Abbot <cpabbot1@gmail.com>
2018-08-03 17:21:00 -0400

    upload and retrieve basic working now using firestore and cloudinary, testing if camera feature works

--- js/upload.js
+++ js/upload.js
@@ -1,46 +1,49 @@
-var selectedFile;
+var fileRef;
 
-$(document).ready(function() {
-    $(".upload-submit").hide();
-//    $(".upload-btn").cloudinary_upload_widget({
-//        cloud_name: 'cpabbot', upload_preset: 'avynotor', multiple: false, cropping: 'server', folder: 'user_photos', button_caption: "Upload", thumnails: '.hidden', stylesheet: 'http://127.0.0.1:59538/css/upload-theme.css' },
-//    function(error, result) { console.log(error, result) });
+document.getElementById("upload_widget_opener").addEventListener("click", function() {
+    cloudinary.openUploadWidget({ cloud_name: 'whiteoakproject', upload_preset: 'whiteoakprojectpreset', folder: 'tree registry', stylesheet: 'http://cameronabbot.ml/white-oak/css/upload-theme.css'}, 
+      function(error, data) {
+//        console.log("file uploaded successfully", error, data);
+//        console.log(data.secure_url);
+//        console.log(data.0);
+        
+//        http://cameronabbot.ml/white-oak/css/upload-theme.css
+//        http://127.0.0.1:65289/css/upload-theme.css
+    });
+  }, false);
+
+$(document).on('cloudinarywidgetfileuploadsuccess', function(e, data) {
+    console.log("Single file success", e, data);
+    fileRef = data.secure_url;
+    console.log(fileRef);
+    upload(fileRef);
 });
 
-$(".upload-input").on("change", function(event) {
-    selectedFile = event.target.files[0];
-    $(".upload-submit").show();
-})
-
-function uploadFile() {
-    var fileName = selectedFile.name;
-    var storageRef = firebase.storage().ref("/trees/" + fileName);
-    var uploadTask = storageRef.put(selectedFile);
+function upload(fileRef) {
+    // Initialize Cloud Firestore through Firebase
+    var db = firebase.firestore();
+    var photosRef = db.collection("photos");
     
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on('state_changed', function(snapshot){
-    // Observe state change events such as progress, pause, and resume
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-    case firebase.storage.TaskState.PAUSED: // or 'paused'
-      console.log('Upload is paused');
-      break;
-    case firebase.storage.TaskState.RUNNING: // or 'running'
-      console.log('Upload is running');
-      break;
-    }
-    }, function(error) {
-    // Handle unsuccessful uploads
-    }, function() {
-    // Handle successful uploads on complete
-    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-    console.log('File available at', downloadURL);
-    });
+    photosRef.add({
+        timestamp: "06/13/2001",
+        name: "a beautiful tree",
+        url: fileRef,
+        retrieve: true
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
     });
 }
+
+
+
+//$(document).ready(function() {
+//    
+//    $(".upload-btn").cloudinary_upload_widget({
+//        cloud_name: 'whiteoakproject', upload_preset: 'whiteoakprojectpreset', multiple: true, cropping: 'server', folder: 'user_photos', button_caption: "Upload", thumnails: '.hidden', stylesheet: 'http://127.0.0.1:59538/css/upload-theme.css' },
+//    function(error, result) { console.log(error, result) });
+//    
+//});
